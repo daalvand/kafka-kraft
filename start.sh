6223c1165a5320b0ba99d6cb02498113ca6fe04b
Mehdi <mdaalvand@gmail.com>
2023-11-16 16:48:15 +0330

    Optimized Kafka setup for increased reliability
    Upgraded the Kafka Docker setup from a single standalone instance to a three-node cluster to enhance system's redundancy and scalability. The environment variable `KAFKA_KRAFT_CLUSTER_ID` has been unwrapped to `CLUSTER_ID` to broaden its usability. Customization of the Kafka settings ensures the cluster operation supported with accurate listeners and node identification. Files `.env.example` and `start.sh` have been adjusted to mirror these changes. The Schema registry service has been added in anticipation of Avro schema support and an auxiliary network, `kafka-kraft`, has been created for the efficient interaction between Kafka nodes and Schema registry. This revamp aims to boost Kafka's fault-tolerance, lower the risk of data loss, and provide an efficient development environment.

--- start.sh
+++ start.sh
@@ -10,19 +10,19 @@ fi
 # Load environment variables from .env file
 export $(cat .env | sed 's/#.*//g' | xargs)
 
-# Check if the KAFKA_KRAFT_CLUSTER_ID variable exists
-if [ -z "$KAFKA_KRAFT_CLUSTER_ID" ]
+# Check if the CLUSTER_ID variable exists
+if [ -z "$CLUSTER_ID" ]
 then
   # Start a temporary Kafka container to generate a new cluster ID
   CLUSTER_ID=$(docker-compose run --rm kafka-1 kafka-storage.sh random-uuid)
 
   # Replace the cluster ID in the .env file
-  sed -i "s/KAFKA_KRAFT_CLUSTER_ID=/KAFKA_KRAFT_CLUSTER_ID=$CLUSTER_ID/" .env
+  sed -i "s/CLUSTER_ID=/CLUSTER_ID=$CLUSTER_ID/" .env
 
   # Export the cluster ID as an environment variable
-  export KAFKA_KRAFT_CLUSTER_ID=$CLUSTER_ID
+  export CLUSTER_ID=$CLUSTER_ID
 else
-  echo "KAFKA_KRAFT_CLUSTER_ID already exists: $KAFKA_KRAFT_CLUSTER_ID"
+  echo "CLUSTER_ID already exists: $CLUSTER_ID"
 fi
 
 # Start the Kafka brokers
